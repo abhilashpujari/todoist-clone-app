76ce631f87d2af328d8b5f37bcedb32cd5349cc5
abhilash <abhilashpujari11@gmail.com>
2019-09-18 12:55:02 +0530

    Added user registration

--- server/src/graphql/resolvers/user.js
+++ server/src/graphql/resolvers/user.js
@@ -3,8 +3,20 @@ export default {
     login: (parent, args, { models }) => {
       return true;
     },
-    register: async (parent, args, { models }) => {
-      return true;
+    register: async (parent, { email, password }, { models }) => {
+      const user = await models.Users.findOne({ email });
+
+      if (user) {
+        throw new Error("User already exits");
+      }
+
+      const newUser = await models.Users.create({
+        email,
+        password
+      });
+
+      console.log(newUser);
+      return newUser;
     }
   }
 };
--- server/src/models/users.js
+++ server/src/models/users.js
@@ -1,4 +1,6 @@
 "use strict";
+var bcrypt = require("bcrypt");
+
 export default (sequelize, DataTypes) => {
   const Users = sequelize.define(
     "Users",
@@ -8,8 +10,28 @@ export default (sequelize, DataTypes) => {
       email: DataTypes.STRING,
       password: DataTypes.STRING
     },
-    {}
+    {
+      instanceMethods: {
+        comparePassword: function(password) {
+          if (bcrypt.compareSync(password, this.password)) {
+            return this;
+          } else {
+            return false;
+          }
+        }
+      }
+    }
   );
+
+  async function encryptPasswordIfChanged(user, options) {
+    if (user.changed("password")) {
+      user.password = await bcrypt.hash(user.password, 12);
+    }
+  }
+
+  Users.beforeCreate(encryptPasswordIfChanged);
+  Users.beforeUpdate(encryptPasswordIfChanged);
+
   Users.associate = function(models) {
     Users.hasMany(models.Projects);
   };
